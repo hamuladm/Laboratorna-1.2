9d5e0f1aa37f2440ef23dd7241afad37d121bfbf
dmytro <hamula.dm@gmail.com>
2023-02-17 18:31:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+'''
+Laboratorna 1
+'''
+
+import folium
+import geopy
+import geocoder
+import argparse
+from typing import List
+
+def read(path: str) -> List:
+    '''
+    (str) -> List
+    Reads dataset
+    path: path to dataset
+    '''
+    try:
+        return []
+    except:
+        return []
+
+def cut(path: str) -> List:
+    '''
+    (str) -> List
+    Cuts dataset
+    path: path to dataset
+    '''
+    try:
+        return []
+    except:
+        return []
+
+def main(path: str) -> 0:
+    '''
+    (str) -> 0
+    Builds HTML-map
+    path: path to dataset
+    '''
+    return 0
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
